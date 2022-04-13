50fe8bebcbe8ca8c3bb99718d27301bb2e09156a
Андрей <79538233369@ya.ru>
2022-04-13 19:42:40 +0500

    2.2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,14 +1,22 @@
 package ru.skypro;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("2.1");
-        int clientOS = 0;
-        if (clientOS == 0) {
-            System.out.println("Install the app for IOS, please. Here is the link..");
-        } else if (clientOS == 1) {
-            System.out.println("Install the app for Android, please. Here is the link..");
+        System.out.println("2.2");
+        int clientDeviceYear = 2015;
+        int OS = 0;
+        boolean oldIOS = clientDeviceYear < 2015 && OS == 0;
+        boolean oldAndroid = clientDeviceYear < 2015 && OS == 1;
+        if (oldIOS) {
+            System.out.println("Install the light version of the app for IOS. Here is the link..");
+        } else if (OS == 0) {
+            System.out.println("Install the app for IOS. Here is the link..");
+        }
+        if (oldAndroid) {
+            System.out.println("Install the light version of the app for Android. Here is the link..");
+        } else if (OS == 1) {
+            System.out.println("Install the app for Android. Here is the link..");
         }
     }
 }
+
