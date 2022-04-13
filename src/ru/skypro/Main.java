f097c51ca0228167a5991d2050e4e819dcd0c1b0
Андрей <79538233369@ya.ru>
2022-04-13 19:28:58 +0500

    2.1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,12 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("2.1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Install the app for IOS, please. Here is the link..");
+        } else if (clientOS == 1) {
+            System.out.println("Install the app for Android, please. Here is the link..");
+        }
     }
 }
