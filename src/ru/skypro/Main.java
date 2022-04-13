e9fe88e834a70fdc839ea01e30d0d3b00f5fb779
Андрей <79538233369@ya.ru>
2022-04-13 19:46:51 +0500

    2.3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,20 +2,14 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("2.2");
-        int clientDeviceYear = 2015;
-        int OS = 0;
-        boolean oldIOS = clientDeviceYear < 2015 && OS == 0;
-        boolean oldAndroid = clientDeviceYear < 2015 && OS == 1;
-        if (oldIOS) {
-            System.out.println("Install the light version of the app for IOS. Here is the link..");
-        } else if (OS == 0) {
-            System.out.println("Install the app for IOS. Here is the link..");
-        }
-        if (oldAndroid) {
-            System.out.println("Install the light version of the app for Android. Here is the link..");
-        } else if (OS == 1) {
-            System.out.println("Install the app for Android. Here is the link..");
+        System.out.println("2.3");
+        int year = 4;
+        year = year+4;
+        boolean leap = year == 400&&year!=100;
+        if (leap) {
+            System.out.println("This year is leap");
+        }else if (year == 3) {
+            System.out.println("This year is not leap");
         }
     }
 }
