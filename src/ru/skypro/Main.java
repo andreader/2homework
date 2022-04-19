032fdda7a53c1e6c091f8947694ac7791566b79a
Андрей <79538233369@ya.ru>
2022-04-19 19:35:59 +0500

    2.3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,14 +3,17 @@ package ru.skypro;
 public class Main {
     public static void main(String[] args) {
         System.out.println("2.3");
-        int year = 4;
-        year = year+4;
-        boolean leap = year == 400&&year!=100;
-        if (leap) {
-            System.out.println("This year is leap");
-        }else if (year == 3) {
-            System.out.println("This year is not leap");
+        int year = 4400;
+        boolean NotLeapYear = year%100 == 0 && year%400 != 0;
+        boolean LeapYear = year%4 == 0;
+        if (NotLeapYear) {
+            System.out.println("The year is not leap");
+        } else if (LeapYear) {
+            System.out.println("The year is leap");
+        }else {
+            System.out.println("The year is not leap");
+        }
         }
     }
-}
+
 
