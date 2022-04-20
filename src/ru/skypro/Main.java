f3a9d993fb01ac8c1a1ee48615fd7af0c976a87e
Андрей <79538233369@ya.ru>
2022-04-20 15:19:43 +0500

    2.5

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,21 +2,54 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("2.4");
-        int deliveryDistance = 95;
-        boolean zone1 = (deliveryDistance < 20);
-        boolean zone2 = (deliveryDistance >= 20 && deliveryDistance < 60);
-        boolean zone3 = (deliveryDistance >= 60 && deliveryDistance < 100);
-        if (zone1) {
-            System.out.println("It takes 1 day" + " (delivery time)");
+        System.out.println("2.5");
+        final int MONTHNUMBER = 10,
+                January = 1, February = 2, March = 3,
+                April = 4, May = 5, June = 6,
+                July = 7, August = 8, September = 9,
+                October = 10, November = 11, December = 12;
+        switch (MONTHNUMBER) {
+            case December:
+                System.out.println("It's winter, December!");
+                break;
+            case January:
+                System.out.println("It's winter, January!");
+                break;
+            case February:
+                System.out.println("It's winter, February!");
+                break;
+            case March:
+                System.out.println("It's spring, March!");
+                break;
+            case April:
+                System.out.println("It's spring, April!");
+                break;
+            case May:
+                System.out.println("It's spring, May!");
+                break;
+            case June:
+                System.out.println("It's summer, June!");
+                break;
+            case July:
+                System.out.println("It's summer, July!");
+                break;
+            case August:
+                System.out.println("It's summer, August!");
+                break;
+            case September:
+                System.out.println("It's autumn, September!");
+                break;
+            case October:
+                System.out.println("It's autumn, October!");
+                break;
+            case November:
+                System.out.println("It's autumn, November!");
+                break;
+            default:
+                System.out.println("Please, enter the correct month value 1-12!");
         }
-        if (zone2) {
-            System.out.println("It takes 2 days" + " (delivery time)");
-        }
-        if (zone3) {
-            System.out.println("It takes 3 days" + " (delivery time)");
         }
     }
-}
+
 
 
