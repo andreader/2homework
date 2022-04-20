3466b517e4acc299496c011637c89172e636b3eb
Андрей <79538233369@ya.ru>
2022-04-20 14:34:52 +0500

    2.4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,8 +3,20 @@ package ru.skypro;
 public class Main {
     public static void main(String[] args) {
         System.out.println("2.4");
-
+        int deliveryDistance = 95;
+        boolean zone1 = (deliveryDistance < 20);
+        boolean zone2 = (deliveryDistance >= 20 && deliveryDistance < 60);
+        boolean zone3 = (deliveryDistance >= 60 && deliveryDistance < 100);
+        if (zone1) {
+            System.out.println("It takes 1 day" + " (delivery time)");
+        }
+        if (zone2) {
+            System.out.println("It takes 2 days" + " (delivery time)");
+        }
+        if (zone3) {
+            System.out.println("It takes 3 days" + " (delivery time)");
         }
     }
+}
 
 
